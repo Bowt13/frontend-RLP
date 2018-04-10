5e5ed92853e56c330b1177d9febadbec3809df9f
evagolabek <golabek.eva@gmail.com>
2018-04-10 16:47:26 +0200

    login signup

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 import LoginPage from './components/LoginPage'
+import SignupPage from './components/SignupPage'
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-          <Route exact path="/login" component={LoginPage} />
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <Router>
+      <div>
+        <Route exact path="/login" component={LoginPage} />
+        <Route exact path="/signup" component={SignupPage} />
       </div>
+      </Router>
     );
   }
 }
---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+import signup from './signup'
+import login from './login'
+import currentUser from './currentUser'
+
+export default {
+  signup,
+  login,
+  currentUser
+}
