6899b26a1e7e3bcc7c17bf87b46770f2584d9055
evagolabek <golabek.eva@gmail.com>
2018-04-10 15:28:57 +0200

    login signup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import LoginPage from './components/LoginPage'
 import logo from './logo.svg';
 import './App.css';
 
@@ -9,6 +11,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
+          <Route exact path="/login" component={LoginPage} />
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
