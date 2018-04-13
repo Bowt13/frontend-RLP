2694a30249515fe6595e9dc4570b2963c6a6e735
Safisyx <safisyx@gmail.com>
2018-04-13 02:43:46 +0200

    Setup socket.io, change localStorage to sessionStorage, change port

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
-export const baseUrl = 'http://localhost:4000'
+export const baseUrl = process.env.SERVER_URL || 'http://localhost:4001'
 
-export const localStorageJwtKey = 'currentUserJwt'
+export const sessionStorageJwtKey = 'currentUserJwt'
