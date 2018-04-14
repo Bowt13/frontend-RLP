7174d52410803f502b1669027689453d0d85055d
evagolabek <golabek.eva@gmail.com>
2018-04-14 19:15:11 +0200

    change contact page to order page plus few details on it

--- src/App.js
+++ src/App.js
@@ -15,7 +15,8 @@
     import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
 //Components
-  import ContactPage from './components/ContactPage'
+  import OrdersPage from './components/OrdersPage'
+  import OrderDetails from './components/OrderDetails'
   import NavBar from './components/NavBar'
 
 
@@ -32,7 +33,9 @@ class App extends Component {
           <Route exact path="/flexicon/create/order" component={ OrderCreator } />
           <Route exact path="/flexicon/InternalContactOverview" component={ InternalContactOverview } />
           <Route exact path="/forgotpassword" component={ ForgotPassword }/>
-          <Route exact path="/flexicon/users/:usersId" component={ ContactPage }/>
+          <Route exact path="/flexicon/orders" component={ OrdersPage }/>
+          <Route exact path="/flexicon/orders/:orderId" component={ OrderDetails }/>
+
           <Route exact path="/" render={ () => <Redirect to="/login" /> } />
         </div>
       </MuiThemeProvider>
