b703cbc666dbe3478e315767b06225957e71127e
evagolabek <golabek.eva@gmail.com>
2018-04-15 14:13:13 +0200

    orders reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import signup from './signup'
 import login from './login'
 import currentUser from './currentUser'
+import orders from './orders'
 
 export default {
   signup,
   login,
-  currentUser
+  currentUser,
+  orders
 }
