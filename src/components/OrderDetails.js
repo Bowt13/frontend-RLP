9425ca3bb93c4932297b49c00aa984feeba2a0d5
Julia Schneider <SchneiderJu@gmx.net>
2018-04-16 14:30:35 +0200

    Update OrderDetails.js

--- src/components/OrderDetails.js
+++ src/components/OrderDetails.js
@@ -64,8 +64,8 @@ class OrderDetails extends PureComponent {
     let windowWidth = window.screen.availWidth
     console.log(windowWidth);
 
-     // const {order} = this.props
-     // if (!order) return <p>order does not exist!</p>
+    const {order} = this.props
+     if (!order) return <p>order does not exist!</p>
 
 		return (
       <div>
