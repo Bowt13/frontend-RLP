394eb233bec7474ffb73ebaf6ff29036c6ad33ce
Ju111 <SchneiderJu@gmx.net>
2018-04-16 14:51:10 +0200

    sort customer email adresses alphabetically

--- src/containers/CustomerList.js
+++ src/containers/CustomerList.js
@@ -4,52 +4,14 @@ import { connect } from 'react-redux'
 
 //MaterialUI
   //Components
-    import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText, CardContent} from 'material-ui/Card';
-    import RaisedButton from 'material-ui/RaisedButton'
+    // import RaisedButton from 'material-ui/RaisedButton'
     import Paper from 'material-ui/Paper';
     import { List, ListItem } from 'material-ui/List';
     import Subheader from 'material-ui/Subheader';
     import Divider from 'material-ui/Divider';
-    import Business from 'material-ui/svg-icons/communication/business'
-
 
 //Actions
 import { getCustomers } from '../actions/users'
-
-//Components
-
-//HArdCode
-
-const orders = [
-  {
-    id: 1,
-    orderNumber: '0707',
-    type: 'Secretary of the Year',
-    description: '3D crystal glass',
-    user: 'Johnny Bravo'
-  },
-  {
-    id: 2,
-    orderNumber: '0808',
-    type: 'Manager of the Month',
-    description: 'Award Tombstone Rectangle',
-    user: 'Annette Whilmore'
-  },
-  {
-    id: 3,
-    orderNumber: '0909',
-    type: 'Coffee Maker of the Week',
-    description: 'Award in Metal and Wood',
-    user: 'Stuart Belleville'
-  },
-  {
-    id: 4,
-    orderNumber: '1010',
-    type: 'Colleague of the Day',
-    description: 'Plexiglass Star Shape',
-    user: 'Johnny Bravo'
-  }
-]
 
 class CustomerList extends PureComponent {
 
@@ -90,9 +52,9 @@ class CustomerList extends PureComponent {
                 textAlign: 'left',
               }}
               secondaryTextLines={2}
-              primaryText={`${customer.companyName}`}
-              secondaryText={<p> <span> {'Nr: '+ `${customer.orderNumber}`}</span><br/><span>{'Created by: '+ `${customer}`}</span></p>}
-              className='order-row' onClick={_=>window.location.href=`/flexicon/orders/${customer.orderNumber}`}
+              primaryText={`${customer.email}`}
+              secondaryText={<p> <span> {`${customer.companyName}`}</span><br/><span>{`${customer.firstName}`+ `${customer.lastName}`}</span></p>}
+              onClick={_=>window.location.href=`/flexicon/orders/${customer.orderNumber}`}
               />
             </div>
           ))
@@ -106,7 +68,7 @@ class CustomerList extends PureComponent {
 
 const mapStateToProps = function (state) {
 	return {
-    customers: state.customers
+    customers: Object.values(state.customers).sort((a, b) => a.email.localeCompare(b.email))
 	}
 }
 
