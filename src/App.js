8ce766e51349b08879fbc5759fe94979d64645f8
evagolabek <golabek.eva@gmail.com>
2018-04-17 14:50:33 +0200

    contact creator start

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@
   import SignupPage from './containers/SignupPage'
   import OrderCreator from './containers/OrderCreatorPage'
   import CustomerList from './containers/CustomerList'
+  import ContactCreator from './components/ContactCreator'
 
 //MATERIALUI
   //PROVIDER
@@ -31,6 +32,7 @@ class App extends Component {
           <Route exact path="/signup/:jwt" component={ SignupPage } />
           <Route exact path="/forgotpassword/:jwt" component={ SignupPage } />
           <Route exact path="/flexicon/create/order" component={ OrderCreator } />
+          <Route exact path="/flexicon/create/contact" component={ ContactCreator } />
           <Route exact path="/flexicon/InternalContactOverview" component={ InternalContactOverview } />
           <Route exact path="/flexicon/orders" component={ OrdersPage }/>
           <Route exact path="/flexicon/customers" component={ CustomerList } />
---
+++ src/components/ContactCreator.js
@@ -0,0 +1,158 @@
+//Dependencies
+import React, {PureComponent} from 'react'
+import {connect} from 'react-redux'
+import {Redirect} from 'react-router-dom'
+import PropTypes from 'prop-types'
+
+//MaterialUI
+  //Components
+    import TextField from 'material-ui/TextField';
+    import RaisedButton from 'material-ui/RaisedButton'
+    import Paper from 'material-ui/Paper';
+    import Divider from 'material-ui/Divider';
+
+  //Icons
+    import Account from 'material-ui/svg-icons/action/account-circle'
+    import Business from 'material-ui/svg-icons/communication/business'
+    import Phone from 'material-ui/svg-icons/communication/phone'
+    import Adres from 'material-ui/svg-icons/maps/add-location'
+    import City from 'material-ui/svg-icons/social/location-city'
+    import Country from 'material-ui/svg-icons/social/public'
+
+  //Colors
+
+
+  //Actions
+
+
+class contactCreator extends PureComponent {
+  static propTypes = {
+    onChange: PropTypes.func.isRequired,
+  }
+  state = {
+  }
+
+  handleChange = (event) => {
+    const {name, value} = event.target
+    this.setState({
+      [name]: value
+    })
+  }
+
+  handleSubmit = () => {
+    this.props.onChange('Create Contact', this.state)
+  }
+
+  // componentDidMount() {
+  //   this.props.onRef(this)
+  // }
+
+	render() {
+
+		return (
+      <div Paper style={{
+        position: 'relative',
+        top: 80,
+        botom: 10,
+        margin: 'auto',
+        width: '50%',
+        overflow: 'scroll',
+      }}>
+      <h1
+        style={{
+          position: 'relative',
+          top: 10,
+        }}
+      >Create Contact</h1>
+      <Divider/>
+        <form
+          name='Create Contact'
+        >
+
+
+        <TextField
+          floatingLabelFocusStyle={{
+            color: '#F09517',
+          }}
+          underlineFocusStyle={{
+            borderColor: '#F09517',
+          }}
+          firstName='First Name'
+          floatingLabelText="First Name:"
+          value={this.state.Bedrijfsnaam || ''}
+          onChange={this.handleChange}
+        />
+        <br/>
+
+
+        <TextField
+          floatingLabelFocusStyle={{
+            color: '#F09517',
+          }}
+          underlineFocusStyle={{
+            borderColor: '#F09517',
+          }}
+          lastName='Last Name'
+          floatingLabelText="Last Name:"
+          value={this.state.Contactpersoon || ''}
+          onChange={this.handleChange}
+        />
+        <br/>
+
+
+        <TextField
+          floatingLabelFocusStyle={{
+            color: '#F09517',
+          }}
+          underlineFocusStyle={{
+            borderColor: '#F09517',
+          }}
+          name='emailAdres'
+          floatingLabelText="Email Adres:"
+          value={this.state.emailAdres || ''}
+          onChange={this.handleChange}
+        />
+        <br/>
+
+        <TextField
+          floatingLabelFocusStyle={{
+            color: '#F09517',
+          }}
+          underlineFocusStyle={{
+            borderColor: '#F09517',
+          }}
+          name='Telefoonnummer'
+          floatingLabelText="Telefoonnummer:"
+          value={this.state.Telefoonnummer || ''}
+          onChange={this.handleChange}
+          style={{
+            marginBottom: 10,
+          }}
+        />
+
+        <TextField
+          floatingLabelFocusStyle={{
+            color: '#F09517',
+          }}
+          underlineFocusStyle={{
+            borderColor: '#F09517',
+          }}
+          name='Role'
+          floatingLabelText="Role:"
+          value={this.state.Role || ''}
+          onChange={this.handleChange}
+        />
+        <br/>
+        <br/>
+        </form>
+      </div>
+		)
+	}
+}
+
+const mapStateToProps = function (state) {
+	return {
+	}
+}
+
+export default connect(mapStateToProps)(contactCreator)
