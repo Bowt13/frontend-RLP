715c026d18840d974be31f3cf4160d5a3d99115a
evagolabek <golabek.eva@gmail.com>
2018-04-17 23:19:28 +0200

    material ui add

--- src/containers/ContactCreator.js
+++ src/containers/ContactCreator.js
@@ -2,30 +2,18 @@
 import React, {PureComponent} from 'react'
 import {connect} from 'react-redux'
 import {Redirect} from 'react-router-dom'
-// import PropTypes from 'prop-types'
 
 //MaterialUI
   //Components
-    // import TextField from 'material-ui/TextField';
-    // import RaisedButton from 'material-ui/RaisedButton'
-    import Paper from 'material-ui/Paper';
-  //   import Divider from 'material-ui/Divider';
-  //   import Checkbox from 'material-ui/Checkbox';
-  //   import Subheader from 'material-ui/Subheader';
-  //
-  // //Icons
-  //   import Account from 'material-ui/svg-icons/action/account-circle'
-  //   import Business from 'material-ui/svg-icons/communication/business'
-  //   import Phone from 'material-ui/svg-icons/communication/phone'
-  //   import Adres from 'material-ui/svg-icons/maps/add-location'
-  //   import City from 'material-ui/svg-icons/social/location-city'
-  //   import Country from 'material-ui/svg-icons/social/public'
-  //
-  // //Colors
+import Paper from 'material-ui/Paper';
+import Subheader from 'material-ui/Subheader';
+import Divider from 'material-ui/Divider';
+import TextField from 'material-ui/TextField';
+import Checkbox from 'material-ui/Checkbox';
+import RaisedButton from 'material-ui/RaisedButton'
 
-
-  //Actions
-  import {createContact} from '../actions/contacts'
+//Actions
+import {createContact} from '../actions/contacts'
 
 // const styles = {
 //   block: {
@@ -59,102 +47,64 @@ class contactCreator extends PureComponent {
 
   return (
     <div style={{ textAlign: 'center'}}>
-    <Paper style={{
-      position: 'relative',
-      top: 80,
-      botom: 10,
-      margin: 'auto',
-      width: '50%',
-      overflow: 'scroll',
-    }}>
-
-    <Subheader style={{
-      fontSize: 40,
-    }}>Create Contact</Subheader>
-    <Divider style={{
-      padding: 1,
-      marginBottom: 1,
-    }}/>
-      <form
-        name='Create Contact'
-      >
-      <TextField
-        floatingLabelFocusStyle={{
-          color: '#F09517',
-        }}
-        underlineFocusStyle={{
-          borderColor: '#F09517',
-        }}
-        firstName='First Name'
-        floatingLabelText="First Name:"
-        value={this.state.firstName || ''}
-        onChange={this.handleChange}
-      />
-      <br/>
-
-
-      <TextField
-        floatingLabelFocusStyle={{
-          color: '#F09517',
-        }}
-        underlineFocusStyle={{
-          borderColor: '#F09517',
-        }}
-        lastName='Last Name'
-        floatingLabelText="Last Name:"
-        value={this.state.lastName || ''}
-        onChange={this.handleChange}
-      />
-      <br/>
-
-
-      <TextField
-        floatingLabelFocusStyle={{
-          color: '#F09517',
-        }}
-        underlineFocusStyle={{
-          borderColor: '#F09517',
-        }}
-        name='emailAdres'
-        floatingLabelText="Email Adres:"
-        value={this.state.emailAdres || ''}
-        onChange={this.handleChange}
-      />
-      <br/>
-
-      <TextField
-        floatingLabelFocusStyle={{
-          color: '#F09517',
-        }}
-        underlineFocusStyle={{
-          borderColor: '#F09517',
-        }}
-        name='telephoneNumber'
-        floatingLabelText="Telephone Number:"
-        value={this.state.telephoneNumber || ''}
-        onChange={this.handleChange}
-        style={{
-          marginBottom: 10,
-        }}
-      />
-      <br/>
-
-      <div style={{display: "inline-block",textAlign:"center"}}>
-        <Checkbox
-          label="Internal"
-        iconStyle={{
-          fill: '#F09517'
-        }}
-        />
-        <br/>
-        <br/>
-      </div>
-
-      <div style={{display: "inline-block"}}>
-      <RaisedButton label="Save"/>
-      </div>
-      <ContactForm onSubmit={this.creataContact}/>
-      </form>
+      <Paper style={{
+        position: 'relative',
+        top: 80,
+        botom: 10,
+        margin: 'auto',
+        width: '50%',
+        overflow: 'scroll',
+      }}>
+        <Subheader style={{ fontSize: 40 }}>Create Contact</Subheader>
+        <Divider style={{ padding: 1, marginBottom: 1 }}/>
+        <form name='Create Contact' onSubmit={this.handleSubmit}>
+          <TextField
+            floatingLabelFocusStyle={{ color: '#F09517' }}
+            underlineFocusStyle={{ borderColor: '#F09517' }}
+            name='firstName'
+            floatingLabelText="First Name:"
+            value={this.state.firstName || ''}
+            onChange={this.handleChange}
+          />
+          <br/>
+          <TextField
+            floatingLabelFocusStyle={{ color: '#F09517' }}
+            underlineFocusStyle={{ borderColor: '#F09517' }}
+            name='lastName'
+            floatingLabelText="Last Name:"
+            value={this.state.lastName || ''}
+            onChange={this.handleChange}
+          />
+          <br/>
+          <TextField
+            floatingLabelFocusStyle={{ color: '#F09517' }}
+            underlineFocusStyle={{ borderColor: '#F09517' }}
+            name='email'
+            floatingLabelText="Email:"
+            value={this.state.email || ''}
+            onChange={this.handleChange}
+          />
+          <br/>
+          <TextField
+            floatingLabelFocusStyle={{ color: '#F09517' }}
+            underlineFocusStyle={{ borderColor: '#F09517' }}
+            name='telephoneNumber'
+            floatingLabelText="Telephone Number:"
+            value={this.state.telephoneNumber || ''}
+            onChange={this.handleChange}
+            style={{ marginBottom: 10 }}
+          />
+          <br/>
+          <div style={{ display: "inline-block", textAlign:"center" }}>
+            <Checkbox label="Internal" iconStyle={{ fill: '#F09517' }}/>
+          </div>
+          <br/>
+          <br/>
+          <br/>
+          <div style={{display: "inline-block"}}>
+            <RaisedButton type="submit" label="Save"/ >
+          </div>
+        </form>
       </Paper>
     </div>
     )
@@ -182,6 +132,8 @@ class contactCreator extends PureComponent {
 //     <button type="submit">Save</button>
 //   </form>
 // </Paper>
+
+// <RaisedButton label="Save"/>
 
 const mapStateToProps = function (state) {
 	return {
