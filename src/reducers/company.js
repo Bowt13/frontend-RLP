f1b5fa19cc91d530d691e95f02520c4d04458555
Ju111 <SchneiderJu@gmx.net>
2018-04-19 12:18:04 +0200

    remove action from reducer

--- src/reducers/company.js
+++ src/reducers/company.js
@@ -5,8 +5,6 @@ export default function (state = [], action) {
   switch (action.type) {
     case GET_COMPANIES:
       return action.payload
-    case ADD_COMPANY:
-      return [...state, action.payload]
     case ADD_CONTACT:
       return state.map(company=>{
         if(action.payload.id===company.id)
